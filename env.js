2e9b9ee15e2c8eed2b249b3103790eb7b9da9b7e
lighofzion <ibacademy2024@gmail.com>
2025-01-20 23:01:45 +0530

    QWEWQEDSDF

--- env.js
+++ env.js
@@ -4,13 +4,13 @@ console.log('Environment Variables:');
 const isNode = typeof process !== 'undefined' && process.versions && process.versions.node;
 
 const ENV = {
-  SUPABASE_KEY: isNode ? process.env.SUPABASE_KEY : (typeof window !== 'undefined' ? window.SUPABASE_KEY : 'development'),
-  SUPABASE_URL: isNode ? process.env.SUPABASE_URL : (typeof window !== 'undefined' ? window.SUPABASE_URL : 'http://localhost:3000'),
+  NEXT_PUBLIC_SUPABASE_KEY : isNode ? process.env.NEXT_PUBLIC_SUPABASE_KEY  : (typeof window !== 'undefined' ? window.NEXT_PUBLIC_SUPABASE_KEY  : 'development'),
+  NEXT_PUBLIC_SUPABASE_URL: isNode ? process.env.NEXT_PUBLIC_SUPABASE_URL : (typeof window !== 'undefined' ? window.NEXT_PUBLIC_SUPABASE_URL : 'http://localhost:3000'),
   PORT: isNode ? process.env.PORT : 3000
 };
 
-console.log('SUPABASE_KEY:', ENV.SUPABASE_KEY);
-console.log('SUPABASE_URL:', ENV.SUPABASE_URL);
+console.log('NEXT_PUBLIC_SUPABASE_KEY :', ENV.NEXT_PUBLIC_SUPABASE_KEY );
+console.log('NEXT_PUBLIC_SUPABASE_URL:', ENV.NEXT_PUBLIC_SUPABASE_URL);
 
 // In the browser, you can manually assign window variables or inject them during the build
 if (typeof window !== 'undefined') {
